40a99745ced4d21464fb681438e74a2136d7b96e
JAMDev <jamdeveloper0@gmail.com>
2022-10-13 15:45:14 -0300

    Criação do Transactions

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { TransactionsHeader } from "./components/TransactionsHeader";
+import { TransactionsContainer } from "./styles";
+
+export function Transactions() {
+  return (
+    <TransactionsContainer>
+      <TransactionsHeader />
+    </TransactionsContainer>
+  )
+}
