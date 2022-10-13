6e0b914ec73f89079ae110ba5be1594cbe8ed706
JAMDev <jamdeveloper0@gmail.com>
2022-10-13 15:39:49 -0300

    Inicialização do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+
+    box-shadow: 0px 0px 0px 2px ${params => params.theme['gray-500']};
+  }
+
+  body {
+    background-color: ${params => params.theme['gray-800']};
+    color: ${params => params.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  textarea, button, body, input {
+    font: 400 1rem 'Roboto', sans-serif;
+    line-height: 160%;
+  }
+`
