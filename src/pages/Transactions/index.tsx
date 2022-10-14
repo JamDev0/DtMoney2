2bdb2fda2a194ebdd6ce11b19b97a6dcc56f89df
JAMDev <jamdeveloper0@gmail.com>
2022-10-14 07:46:25 -0300

    Implementação do TransactionsTable

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,13 @@
 import { TransactionsHeader } from './components/TransactionsHeader'
+import { TransactionsTable } from './components/TransactionsTable'
 import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <TransactionsHeader />
+
+      <TransactionsTable />
     </TransactionsContainer>
   )
 }
