1e6b9a0917f2cc7558f7c2562e2b85fd66da6ad3
JAMDev <jamdeveloper0@gmail.com>
2022-10-14 13:16:23 -0300

    Remoção do TransactionsTable do corpo principal do Transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,10 @@
 import { TransactionsHeader } from './components/TransactionsHeader'
-import { TransactionsTable } from './components/TransactionsTable'
 import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <TransactionsHeader />
-
-      <TransactionsTable />
     </TransactionsContainer>
   )
 }
