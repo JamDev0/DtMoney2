49cf9d6f6c4cdbff96e35205eabec8bca4173162
JAMDev <jamdeveloper0@gmail.com>
2022-10-20 17:48:09 -0300

    School updates

--- src/pages/Transactions/components/TransactionsMain/components/TrasactionsSearchForm/index.tsx
+++ src/pages/Transactions/components/TransactionsMain/components/TrasactionsSearchForm/index.tsx
@@ -1,15 +1,34 @@
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import {
   SearchBtn,
   SearchInput,
   TransactionsSearchFormContainer,
 } from './styles'
 
+import * as z from 'zod'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+  query: z.string()
+})
+
+type searchFormInputs = z.infer<typeof searchFormSchema>
+
 export function TransactionsSearchForm() {
+  const { register, handleSubmit } = useForm<searchFormInputs>({
+    resolver: zodResolver(searchFormSchema)
+  })
+
+  function handleTransactionsSearchFormContainer(data: searchFormInputs) {
+
+  }
+
   return (
     <TransactionsSearchFormContainer>
       <SearchInput placeholder="Busque por transações" />
-      <SearchBtn>
+      <SearchBtn {...register('query')}>
         <MagnifyingGlass />
 
         <span>Buscar</span>
